20bbeb7f5b3bc6b22271e2314e60028e92c7edb3
calla <andhikamarcellafernanda@gmail.com>
2025-10-08 12:50:29 +0700

    Update Providers.tsx

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,14 +1,14 @@
 'use client';
 
+import * as React from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { useState } from 'react';
 import { ToastProvider } from '@/hooks/useToast';
-import { Toaster } from './ui/toaster';
+import { Toaster } from '@/components/Toaster'; // biarkan sesuai proyekmu
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = React.useState(() => new QueryClient());
 
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
@@ -17,7 +17,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
           {children}
           <Toaster />
         </ToastProvider>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+
+        {/* Hilangkan prop position biar kompatibel di semua versi */}
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </NextThemesProvider>
   );
